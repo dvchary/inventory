301e94e6e0fb2ed8fe20654f7d8f92f1175c493f
dvchary <varaprasad.achary@gmail.com>
2023-08-01 19:21:58 +0530

    added middlewares

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
 // Connect to DB and Start Server
 mongoose
   .connect(process.env.MONGO_URI)
